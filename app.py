59b6108550f93da11efc79886e4ada976831edfa
Linda Ebbert <linda_ebbert@yahoo.de>
2026-06-30 23:36:03 +0200

    feat(routes): add index route

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from storage.posts_repository import get_posts
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    posts = get_posts()
+    return render_template('index.html', posts=posts)
 
 
 if __name__ == '__main__':
