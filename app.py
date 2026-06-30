b57a8acc8a6770f2b0593ce9b2832b4a4e2c29f6
Linda Ebbert <linda_ebbert@yahoo.de>
2026-07-01 00:49:01 +0200

    feat(routes): add GET/POST add route

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
-from storage.posts_repository import get_posts
+from flask import Flask, render_template, request, redirect, url_for
+
+from storage.posts_repository import get_posts, add_post
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     posts = get_posts()
+    posts.reverse()
     return render_template('index.html', posts=posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        post_author = request.form['author']
+        post_title = request.form.get('title')
+        post_content = request.form.get('content')
+        add_post(post_author, post_title, post_content)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
